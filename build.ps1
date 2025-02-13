d035a9a2dd1e7f77ef6188753e7f9a89ae143731
Leo Liu <11664880+silver886@users.noreply.github.com>
2025-02-12 23:28:26 -0800

    add exe attributes

---
+++ build.ps1
@@ -0,0 +1,40 @@
+$projectName = 'ONEPASS'
+$projectVersion = '0.0.1'
+
+$gui = $false
+$pack = $false
+
+Push-Location $PSScriptRoot
+
+$timestamp = [Xml.XmlConvert]::ToString((Get-Date)) -replace '(\+|-|:|\.)', '' -replace '(Z|\d\d\d\d$)', ''
+
+$outputName = "$($projectName)_$($projectVersion)_$timestamp"
+
+goversioninfo -64 -o "$projectName.syso" "$projectName.json"
+
+dotenvx get --overload --strict -f .\.env.mapleridge --format=eval | ForEach-Object {
+    Invoke-Expression -Command "`$$_"
+}
+
+if ($gui) {
+    if ($pack) {
+        packr build -o "$outputName.exe" -ldflags="-H=windowsgui -s -w -X main.defaultCloudflaredVersion=$CLOUDFLARED_VERSION -X main.defaultCloudflareZeroOrganization=$CLOUDFLARE_ZERO_ORGANIZATION -X main.defaultRemote=$REMOTE -X main.defaultLocal=$LOCAL"
+    }
+    else {
+        go build -o "$outputName.exe" -ldflags="-H=windowsgui -s -w -X main.defaultCloudflaredVersion=$CLOUDFLARED_VERSION -X main.defaultCloudflareZeroOrganization=$CLOUDFLARE_ZERO_ORGANIZATION -X main.defaultRemote=$REMOTE -X main.defaultLocal=$LOCAL"
+    }
+}
+else {
+    if ($pack) {
+        packr build -o "$outputName.exe" -ldflags="-s -w -X main.defaultCloudflaredVersion=$CLOUDFLARED_VERSION -X main.defaultCloudflareZeroOrganization=$CLOUDFLARE_ZERO_ORGANIZATION -X main.defaultRemote=$REMOTE -X main.defaultLocal=$LOCAL"
+    }
+    else {
+        go build -o "$outputName.exe" -ldflags="-s -w -X main.defaultCloudflaredVersion=$CLOUDFLARED_VERSION -X main.defaultCloudflareZeroOrganization=$CLOUDFLARE_ZERO_ORGANIZATION -X main.defaultRemote=$REMOTE -X main.defaultLocal=$LOCAL"
+    }
+}
+
+Remove-Item -Path "$projectName.syso"
+
+upx --ultra-brute "$outputName.exe"
+
+Copy-Item "$outputName.exe" "$projectName.exe"
