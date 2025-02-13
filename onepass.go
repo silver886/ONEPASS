c0623c723a6ef4c380713887256ad1c94cc312bc
Leo Liu <11664880+silver886@users.noreply.github.com>
2025-02-13 12:51:52 -0800

    print login url to log

--- onepass.go
+++ onepass.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/silver886/execute"
+	"github.com/silver886/logger"
 	"github.com/sirupsen/logrus"
 )
 
@@ -26,25 +27,52 @@ func main() {
 	initPost()
 
 	// Login
-	logMain.Warnln("Log in to the tunnel on the page that just popped up")
+	logMain.Warnln("Log in to the tunnel")
 	logMain.Infoln("Get the first remote . . .")
 	firstRemote := arg.Remote
 	if strings.Contains(firstRemote, separator) {
 		firstRemote = strings.Split(firstRemote, separator)[0]
 	}
+	var loginWaitGroup sync.WaitGroup
+	loginWaitGroup.Add(1)
 	if cmd, err := execute.New(
 		cloudflaredPath,
 		"access",
 		"login",
+		"--quiet",
 		firstRemote,
-	).Hide().Run(); err != nil {
+	).Hide().Start(); err != nil {
 		logMain.WithError(err).WithFields(logrus.Fields{
 			"cmd_path":   cmd.Path,
 			"cmd_args":   cmd.Args,
 			"cmd_stdout": cmd.OutString(),
 			"cmd_stderr": cmd.ErrString(),
 		}).Errorln("Cannot login")
+		exit(1234)
 	} else {
+		go func(log *logger.Entry, cmd *execute.Cmd, remote string) {
+			// Set logger prefix
+			logLogin := log.WithField("sub", "login")
+
+			// Get login URL
+			logLogin.Infoln("Get login URL . . .")
+			for msg := cmd.ErrStringNext(); cmd.ProcessState == nil; msg = cmd.ErrStringNext() {
+				if msg != "" {
+					for _, v := range strings.Split(msg, "\n") {
+						if strings.Contains(v, "https://"+remote) {
+							logLogin.WithFields(logrus.Fields{
+								"url": v,
+							}).Warnln("If the browser failed to open, visit the URL below")
+						}
+					}
+				}
+			}
+		}(logMain, cmd, firstRemote)
+		go func() {
+			cmd.Wait()
+			loginWaitGroup.Done()
+		}()
+		loginWaitGroup.Wait()
 		logMain.WithFields(logrus.Fields{
 			"hostname": arg.Remote,
 			"stdout":   cmd.OutString(),
@@ -56,12 +84,19 @@ func main() {
 	logMain.Warnln("Setting up the tunnel")
 	remotes, locals := strings.Split(arg.Remote, separator), strings.Split(arg.Local, separator)
 	logMain.Infoln("Generate wait group . . .")
-	var wg sync.WaitGroup
-	wg.Add(len(remotes))
+	var tunnelWaitGroup sync.WaitGroup
+	tunnelWaitGroup.Add(len(remotes))
 	for i := range remotes {
-		remote := remotes[i]
-		local := locals[i]
-		go func() {
+		go func(log *logger.Entry, remote string, local string) {
+			// Set logger prefix
+			logTunnel := log.WithFields(logrus.Fields{
+				"sub":    "tunnel",
+				"remote": remote,
+				"local":  local,
+			})
+
+			// Create tunnel
+			logTunnel.Infoln("Create tunnel . . .")
 			if cmd, err := execute.New(
 				cloudflaredPath,
 				"access",
@@ -71,30 +106,30 @@ func main() {
 				"--url",
 				local,
 			).Hide().Start(); err != nil {
-				logMain.WithError(err).WithFields(logrus.Fields{
+				logTunnel.WithError(err).WithFields(logrus.Fields{
 					"cmd_path":   cmd.Path,
 					"cmd_args":   cmd.Args,
 					"cmd_stdout": cmd.OutString(),
 					"cmd_stderr": cmd.ErrString(),
 				}).Errorln("Cannot create tunnel")
 			} else {
-				logMain.Debugln("Wait for cloudflared exit . . .")
+				logTunnel.Debugln("Wait for cloudflared exit . . .")
 				cmd.Wait()
-				logMain.WithError(err).WithFields(logrus.Fields{
+				logTunnel.WithError(err).WithFields(logrus.Fields{
 					"hostname": arg.Remote,
 					"url":      arg.Local,
 					"stdout":   cmd.OutString(),
 					"stderr":   cmd.ErrString(),
 				}).Infoln("Tunnel terminated")
 			}
-			wg.Done()
-		}()
+			tunnelWaitGroup.Done()
+		}(logMain, remotes[i], locals[i])
 	}
 
 	// Halt here
 	logMain.Warnln("Let's start")
 	logMain.Infoln("Wait for tunnel terminated . . .")
-	wg.Wait()
+	tunnelWaitGroup.Wait()
 
 	// Exit program
 	exit(0)
