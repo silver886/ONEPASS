f1e07ad4efb5b6aa20cda3018524d712192e0fe6
Leo Liu <11664880+silver886@users.noreply.github.com>
2025-04-28 21:35:36 -0700

    fix build script

--- build.ps1
+++ build.ps1
@@ -10,18 +10,18 @@ Push-Location $PSScriptRoot
 $timestamp = [Xml.XmlConvert]::ToString((Get-Date)) -replace '(\+|-|:|\.)', '' -replace '(Z|\d\d\d\d$)', ''
 $outputName = "$($projectName)_$($projectVersion)_$($timestamp)_{{OS}}_{{ARCH}}"
 
+dotenvx get --overload --strict "--env-file=.env.$projectEnv" --format=eval | ForEach-Object {
+    Invoke-Expression -Command "`$$_"
+}
+
 $cc = 'go'
 $cflags = ''
 $ldflags = "-s -w -X main.toolName=$projectName -X main.toolVersion=$projectVersion -X main.defaultCloudflaredVersion=$CLOUDFLARED_VERSION -X main.defaultRemote=$REMOTE -X main.defaultLocal=$LOCAL"
 if ($gui) {
-    $ldflags = "-H=windowsgui $ldflags"
+    $ldflags = "$ldflags -H=windowsgui $ldflags"
 }
 if ($pack) {
     $cc = 'packr'
-}
-
-dotenvx get --overload --strict "--env-file=.env.$projectEnv" --format=eval | ForEach-Object {
-    Invoke-Expression -Command "`$$_"
 }
 
 & {
